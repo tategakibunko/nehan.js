d3dbb5f944be25f320ed000052f85c2ea37904d4
tategakibunko <lambda.watanabe@gmail.com>
2013-06-10 13:23:09 +0900

    a little optimize

--- example/nehan.js
+++ example/nehan.js
@@ -1355,11 +1355,12 @@ var Utils = {
 };
 
 var reqAnimationFrame = (function(){
+  var default_wait = 1000 / 60;
   return window.requestAnimationFrame  ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame    ||
     function(callback, wait){
-      var _wait = (typeof wait === "undefined")? (1000 / 60) : wait;
+      var _wait = (typeof wait === "undefined")? default_wait : wait;
       window.setTimeout(callback, _wait);
     };
 })();
--- src/anim.js
+++ src/anim.js
@@ -1,9 +1,10 @@
 var reqAnimationFrame = (function(){
+  var default_wait = 1000 / 60;
   return window.requestAnimationFrame  ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame    ||
     function(callback, wait){
-      var _wait = (typeof wait === "undefined")? (1000 / 60) : wait;
+      var _wait = (typeof wait === "undefined")? default_wait : wait;
       window.setTimeout(callback, _wait);
     };
 })();
