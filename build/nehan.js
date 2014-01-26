e903116cf919589f735c19a63c474519c17d90a0
tategakibunko <lambda.watanabe@gmail.com>
2014-01-26 14:25:00 +0900

    get ie11 version in new trident user agent

--- build/nehan.js
+++ build/nehan.js
@@ -160,22 +160,28 @@ var Layout = {
 var Env = (function(){
   var nav = navigator.appName;
   var ua = navigator.userAgent.toLowerCase();
-  var matched = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/);
-  var browser, version, is_transform_enable;
-  if(matched){
-    tmp_match = ua.match(/version\/([\.\d]+)/i);
-    if(tmp_match){
-      matched[2] = tmp_match[1];
-    }
-    browser = matched[1].toLowerCase();
-    version = parseInt(matched[2], 10);
+  var is_pure_trident = ua.indexOf("trident") >= 0 && ua.indexOf("msie") < 0;
+  var browser, version, is_transform_enable, tmp_match;
+  if(is_pure_trident){
+    browser = "msie";
+    tmp_match = ua.match(/rv:([\.\d]+)/i);
+    version = tmp_match? parseInt(tmp_match[1], 10) : "";
   } else {
-    browser = nav.toLowerCase();
-    version = parseInt(navigator.appVersion, 10);
+    var matched = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/);
+    if(matched){
+      tmp_match = ua.match(/version\/([\.\d]+)/i);
+      if(tmp_match){
+	matched[2] = tmp_match[1];
+      }
+      browser = matched[1].toLowerCase();
+      version = parseInt(matched[2], 10);
+    } else {
+      browser = nav.toLowerCase();
+      version = parseInt(navigator.appVersion, 10);
+    }
   }
 
-  var is_trident = ua.indexOf("trident") >= 0 && ua.indexOf("msie") < 0;
-  var is_ie = browser === "msie" || is_trident;
+  var is_ie = browser === "msie";
   var is_win = ua.indexOf("windows") >= 0;
   var is_mac = ua.indexOf("macintosh") >= 0;
   var is_chrome = browser.indexOf("chrome") >= 0;
@@ -186,13 +192,13 @@ var Env = (function(){
   var is_android_family = ua.indexOf("android") != -1;
   var is_smart_phone = is_iphone_family || is_android_family;
   var is_webkit = ua.indexOf("webkit") != -1;
-  var is_transform_enable = is_trident || !(is_ie && version <= 8);
+  var is_transform_enable = is_pure_trident || !(is_ie && version <= 8);
   var is_vertical_glyph_enable = is_chrome && (is_win || is_mac) && version >= 24;
 
   return {
     version : version,
     isIE : is_ie,
-    isTrident : is_trident,
+    isTrident : is_pure_trident,
     isChrome : is_chrome,
     isWebkit : is_webkit,
     isIphone : is_iphone,
