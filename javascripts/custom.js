e374ecf5d8b7f761ac3690741c50856bd6499df3
tategakibunko <lambda.watanabe@gmail.com>
2014-02-17 21:06:30 +0900

    add examples

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,8 +3,26 @@ $(function(){
     usePager:true,
     direction: "hori",
     fontSize:16,
-    width: 640,
-    height: 480,
+    width: 450,
+    height: 400,
+    readerElements:["pager", "screen"],
+    pagerElements:["left-prev", "indicator", "right-next"],
+    useWheel:false,
+    onComplete: function(reader){
+      var outline = reader.getOutlineNode("ul");
+      if(outline){
+	outline.classList.add("book-outline");
+	reader.getRootNode().appendChild(outline);
+      }
+    }
+  });
+
+  $(".nehan-book-vert").nehan({
+    usePager:true,
+    direction: "vert",
+    fontSize:16,
+    width: 450,
+    height: 400,
     readerElements:["pager", "screen"],
     pagerElements:["left-prev", "indicator", "right-next"],
     useWheel:false,
