181125327feeade99a5151b039c547052be52249
tategakibunko <lambda.watanabe@gmail.com>
2014-02-17 18:30:02 +0900

    add demo

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,2 +1,11 @@
 $(function(){
+  $("nehan-book").nehan({
+    usePager:true,
+    direction: "hori",
+    fontSize:16,
+    width: 640,
+    height: 480,
+    pagerElements:["left-prev", "indicator", "right-next"],
+    useWheel:false
+  });
 });
