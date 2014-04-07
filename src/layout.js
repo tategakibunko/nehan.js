621429958de3f4b05db0acbbf969ed89fa25c476
tategakibunko <lambda.watanabe@gmail.com>
2014-04-08 04:45:24 +0900

    add comment

--- src/layout.js
+++ src/layout.js
@@ -1,5 +1,14 @@
 var Layout = {
-  root:"document", // 'body' or 'html' or 'document'
+  // define root where content text starts from.
+  // 'body' or 'html' or 'document' are enabled.
+  // 
+  // 1. 'document'
+  //    content text include <!doctype xxx> tag
+  // 2. 'html'
+  //    content text include <head> and <html>.
+  // 3. 'body'
+  //    content text include <body> or content of body itself.
+  root:"document",
   direction:"vert", // or 'hori'
   hori:"lr-tb", // used when direction is 'hori'. notice that rl-tb is not supported yet.
   vert:"tb-rl", // used when direction is 'vert'. "tb-lr" is also supported.
@@ -7,9 +16,9 @@ var Layout = {
   height: 580, // layout default height if height prop not set in 'body' style.
   fontSize:16, // layout default font-size if font-size prop not set in 'body' style.
   maxFontSize:64,
-  rubyRate:0.5, // used when Style.rt["font-size"] not defined.
+  rubyRate:0.5, // used when Style.rt["font-size"] is not defined.
   boldRate:0.5,
-  lineRate: 2.0, // in nehan.js, extent size of line is specified by [lineRate] * [largest font_size of currentline].
+  lineRate: 2.0, // in nehan.js, extent size of line is specified by [lineRate] * [largest font_size of current line].
 
   // we need to specify these values(color,font-image-root) to display vertical font-images for browsers not supporting vert writing-mode.
   fontColor:"000000",
@@ -19,7 +28,6 @@ var Layout = {
   // these font-fmailies are needed to calculate proper text-metrics.
   vertFontFamily:"'ヒラギノ明朝 Pro W3','Hiragino Mincho Pro','HiraMinProN-W3','IPA明朝','IPA Mincho', 'Meiryo','メイリオ','ＭＳ 明朝','MS Mincho', monospace",
   horiFontFamily:"'Meiryo','メイリオ','Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3','Osaka','ＭＳ Ｐゴシック', monospace",
-  markerFontFamily:"'Meiryo','メイリオ','Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3','Osaka','ＭＳ Ｐゴシック', monospace",
   fontSizeAbs:{
     "xx-large":"33px",
     "x-large":"24px",
@@ -42,7 +50,7 @@ var Layout = {
     return BoxFlows.getByName(this.hori);
   },
   getHoriIndir : function(){
-    return this.hori.split("-")[0];
+    return this.hori.split("-")[0]; // "lr" or "rl"
   },
   getRubyFontSize : function(base_font_size){
     var rt = Style.rt || null;
