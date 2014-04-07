5cae766f01f0fbc10a5a487f515929c948b54dc7
tategakibunko <lambda.watanabe@gmail.com>
2014-04-07 21:01:18 +0900

    rename outline-context-converter.js -> section-tree-converter.js

---
+++ src/section-tree-converter.js
@@ -0,0 +1,104 @@
+/*
+  outline mapping
+  ===============
+
+  <ol>
+    <li> root.title
+      <ol>
+        <li> root.child.title  </li>
+	<li> root.child.next.title </li>
+      </ol>
+    </li>
+
+    <li> root.next.title
+      <ol>
+        <li> root.next.child.title </li>
+	<li> root.next.child.next.title </li>
+      </ol>
+   </li>
+  </ol>
+*/
+// section tree -> dom node
+var SectionTreeConverter = (function(){
+  var default_callbacks = {
+    onClickLink : function(toc){
+      console.log("toc clicked!:%o", toc);
+      return false;
+    },
+    createToc : function(toc_ctx, tree){
+      return {
+	tocPos:toc_ctx.toString(),
+	title:tree.getTitle(),
+	pageNo:tree.getPageNo(),
+	headerId:tree.getHeaderId()
+      };
+    },
+    createRoot : function(toc){
+      var root = document.createElement("ol");
+      root.className = "nehan-toc-root";
+      return root;
+    },
+    createChild : function(toc){
+      var li = document.createElement("li");
+      li.className = "nehan-toc-item";
+      return li;
+    },
+    createLink : function(toc){
+      var link = document.createElement("a");
+      var title = toc.title.replace(/<a[^>]+>/gi, "").replace(/<\/a>/gi, "");
+      link.href = "#" + toc.pageNo;
+      link.innerHTML = title;
+      link.className = "nehan-toc-link";
+      link.id = Css.addNehanTocLinkPrefix(toc.tocId);
+      return link;
+    },
+    createPageNoItem : function(toc){
+      return null;
+    }
+  };
+
+  var parse = function(toc_ctx, parent, tree, callbacks){
+    if(tree === null){
+      return parent;
+    }
+    var toc = callbacks.createToc(toc_ctx, tree);
+    var li = callbacks.createChild(toc);
+    var link = callbacks.createLink(toc);
+    if(link){
+      link.onclick = function(){
+	return callbacks.onClickLink(toc);
+      };
+      li.appendChild(link);
+    }
+    var page_no_item = callbacks.createPageNoItem(toc);
+    if(page_no_item){
+      li.appendChild(page_no_item);
+    }
+    parent.appendChild(li);
+
+    var child = tree.getChild();
+    if(child){
+      toc_ctx = toc_ctx.startRoot();
+      var child_toc = callbacks.createToc(toc_ctx, child);
+      var ol = callbacks.createRoot(child_toc);
+      arguments.callee(toc_ctx, ol, child, callbacks);
+      li.appendChild(ol);
+      toc_ctx = toc_ctx.endRoot();
+    }
+    var next = tree.getNext();
+    if(next){
+      arguments.callee(toc_ctx.stepNext(), parent, next, callbacks);
+    }
+    return parent;
+  };
+
+  return {
+    // section tree -> dom node
+    convert : function(outline_tree, callbacks){
+      callbacks = Args.merge({}, default_callbacks, callbacks || {});
+      var toc_context = new TocContext();
+      var root_node = callbacks.createRoot();
+      return parse(toc_context, root_node, outline_tree, callbacks);
+    }
+  };
+})();
