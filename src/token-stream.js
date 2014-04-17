41cd55befce6ad5800bae0bc1a7bca82005a527a
tategakibunko <lambda.watanabe@gmail.com>
2014-04-17 15:36:04 +0900

    remove unused funcs in token-stream.js

--- src/token-stream.js
+++ src/token-stream.js
@@ -8,12 +8,6 @@ var TokenStream = (function(){
   }
 
   TokenStream.prototype = {
-    _createLexer : function(src){
-      return new HtmlLexer(src);
-    },
-    getSrc : function(){
-      return this.lexer.getSrc();
-    },
     hasNext : function(){
       return (!this.eof || this.pos < this.tokens.length);
     },
@@ -26,36 +20,11 @@ var TokenStream = (function(){
     isHead : function(){
       return this.pos === 0;
     },
-    look : function(index){
-      return this.tokens[index] || null;
-    },
-    next : function(cnt){
-      var count = cnt || 1;
-      this.pos = this.pos + count;
-    },
     prev : function(){
       this.pos = Math.max(0, this.pos - 1);
     },
     setPos : function(pos){
       this.pos = pos;
-    },
-    skipIf : function(fn){
-      var token = this.peek();
-      if(token && fn(token)){
-	this.next();
-      }
-    },
-    skipUntil : function(fn){
-      while(this.hasNext()){
-	var token = this.get();
-	if(token === null){
-	  break;
-	}
-	if(!fn(token)){
-	  this.prev();
-	  break;
-	}
-      }
     },
     rewind : function(){
       this.pos = 0;
@@ -81,19 +50,11 @@ var TokenStream = (function(){
       this.pos++;
       return token;
     },
+    getSrc : function(){
+      return this.lexer.getSrc();
+    },
     getPos : function(){
       return this.pos;
-    },
-    getAll : function(){
-      while(!this.eof){
-	this._doBuffer();
-      }
-      return this.tokens;
-    },
-    getAllIf : function(fn){
-      return List.filter(this.getAll(), function(token){
-	return fn(token);
-      });
     },
     getTokenCount : function(){
       return this.tokens.length;
@@ -105,6 +66,17 @@ var TokenStream = (function(){
     getSeekPercent : function(){
       var seek_pos = this.getSeekPos();
       return this.lexer.getSeekPercent(seek_pos);
+    },
+    getAll : function(){
+      while(!this.eof){
+	this._doBuffer();
+      }
+      return this.tokens;
+    },
+    getAllIf : function(fn){
+      return List.filter(this.getAll(), function(token){
+	return fn(token);
+      });
     },
     getWhile : function(fn){
       var ret = [], token;
@@ -119,77 +91,23 @@ var TokenStream = (function(){
       }
       return ret;
     },
-    // iterate while fn(pos, token) returns true.
-    // so loop is false break
-    iterWhile : function(start_pos, fn){
-      var ptr = (arguments.length == 1)? this.pos : start_pos;
-      while(ptr >= 0){
-	if(ptr >= this.tokens.length){
-	  if(this.eof){
-	    break;
-	  }
-	  this._doBuffer();
-	  this.iterWhile(ptr, fn);
+    skipUntil : function(fn){
+      while(this.hasNext()){
+	var token = this.get();
+	if(token === null){
 	  break;
 	}
-	if(!fn(ptr, this.tokens[ptr])){
+	if(!fn(token)){
+	  this.prev();
 	  break;
 	}
-	ptr++;
       }
     },
-    // reverse iterate while fn(pos, token) returns true.
-    // so loop is false break
-    revIterWhile : function(start_pos, fn){
-      var ptr = (arguments.length == 1)? this.pos : start_pos;
-      while(ptr >= 0){
-	if(!fn(ptr, this.tokens[ptr])){
-	  break;
-	}
-	ptr--;
-      }
-    },
-    findTextPrev : function(start_p){
-      var start_pos = (typeof start_p != "undefined")? start_p : this.pos;
-      var text = null;
-      this.revIterWhile(start_pos - 1, function(pos, token){
-	if(token){
-	  if(!Token.isText(token)){
-            // blocked by recursive inline element.
-            // TODO: get tail element of recursive inline element.
-            return false;
-	  }
-	  text = token;
-	  return false; // break
-	}
-	return false; // break
-      });
-      return text;
-    },
-    findTextNext : function(start_p){
-      var start_pos = (typeof start_p != "undefined")? start_p : this.pos;
-      var text = null;
-      this.iterWhile(start_pos + 1, function(pos, token){
-	if(token){
-	  if(!Token.isText(token)){
-            // blocked by recursive inline element.
-            // TODO: get tail element of recursive inline element.
-            return false;
-	  }
-	  text = token;
-	  return false; // break
-	}
-	return false; // break
-      });
-      return text;
+    _createLexer : function(src){
+      return new HtmlLexer(src);
     },
     _doBuffer : function(){
       var buff_len = Config.lexingBufferLen;
-      /*
-      var self = this;
-      var push = function(token){
-	self.tokens.push(token);
-      };*/
       for(var i = 0; i < buff_len; i++){
 	var token = this.lexer.get();
 	if(token === null){
@@ -197,7 +115,6 @@ var TokenStream = (function(){
 	  break;
 	}
 	this.tokens.push(token);
-	//push(token);
       }
     }
   };
