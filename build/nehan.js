03c26789ba4741bf1d52d5d86a051c1fbe1ff480
tategakibunko <lambda.watanabe@gmail.com>
2014-04-18 11:56:09 +0900

    fix marker size if marker html is empty

--- build/nehan.js
+++ build/nehan.js
@@ -8351,8 +8351,8 @@ var ListGenerator = (function(){
     var max_marker_text = this.style.getMarkerHtml(item_count);
     var gen = new InlineGenerator(this.style, new TokenStream(max_marker_text));
     var line = gen.yield();
-    var marker_measure = line.inlineMeasure + Math.floor(this.style.getFontSize() / 2);
-    var marker_extent = line.size.getExtent(this.style.flow);
+    var marker_measure = line? line.inlineMeasure + Math.floor(this.style.getFontSize() / 2) : this.style.getFontSize();
+    var marker_extent = line? line.size.getExtent(this.style.flow) : this.style.getFontSize();
     return this.style.flow.getBoxSize(marker_measure, marker_extent);
   };
 
