f0e6f756ec380a07479d3e3858314b89914e958c
tategakibunko <lambda.watanabe@gmail.com>
2014-04-25 09:46:24 +0900

    update

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,12 +3,20 @@ $(function(){
     usePager:true,
     direction: "hori",
     fontSize:16,
-    width: 450,
-    height: 400,
+    height: 500,
     readerElements:["pager", "screen"],
     pagerElements:["left-prev", "indicator", "right-next"],
+    horiFontFamily:"'ヒラギノ明朝 Pro W3','Hiragino Mincho Pro','HiraMinProN-W3','IPA明朝','IPA Mincho', 'Meiryo','メイリオ','ＭＳ 明朝','MS Mincho', monospace",
     useWheel:false,
+    onCreateEngine : function(engine){
+      engine.setStyles({
+	"h3":{
+	  color:"green"
+	}
+      });
+    },
     onComplete: function(reader){
+      console.log("reader:%o", reader);
       var outline = reader.getOutlineNode("ul");
       if(outline){
 	outline.classList.add("book-outline");
@@ -21,10 +29,10 @@ $(function(){
     usePager:true,
     direction: "vert",
     fontSize:16,
-    width: 450,
-    height: 400,
+    height: 500,
     readerElements:["pager", "screen"],
     pagerElements:["left-prev", "indicator", "right-next"],
+    horiFontFamily:"'ヒラギノ明朝 Pro W3','Hiragino Mincho Pro','HiraMinProN-W3','IPA明朝','IPA Mincho', 'Meiryo','メイリオ','ＭＳ 明朝','MS Mincho', monospace",
     useWheel:false,
     onComplete: function(reader){
       var outline = reader.getOutlineNode("ul");
