cf49a10fa2943d33866b2610046f6c67a4e1bed6
tategakibunko <lambda.watanabe@gmail.com>
2014-04-27 02:34:48 +0900

    remove some config

--- build/nehan.js
+++ build/nehan.js
@@ -43,11 +43,8 @@ var Config = {
   justify:true,
   maxRollbackCount : 40,
   maxPageCount: 10000,
-  minBlockScaleDownRate : 65,
   useVerticalGlyphIfEnable: true,
   useStrictWordMetrics: true,
-  iboxEnable:false,
-  maxBase:36,
   lexingBufferLen : 2000,
   defaultLinkTitleLength : 16
 };
