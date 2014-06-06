cb2f5af3ceac8d89c1e41dc8a5dd4a51cca3882f
tategakibunko <lambda.watanabe@gmail.com>
2014-06-06 10:23:58 +0900

    add new class hash-set, css-hash-set

---
+++ src/css-hash-set.js
@@ -0,0 +1,19 @@
+var CssHashSet = (function(){
+  function CssHashSet(values){
+    HashSet.prototype.call(this, values || null);
+  }
+  Class.extend(CssHashSet, HashSet);
+
+  // merge css value
+  // 1. if old_value is object, merge each properties.
+  // 2. other case, simplly overwrite new_value to old_value(even if new_value is function).
+  CssHashSet.prototype.merge = function(old_value, new_value){
+    if(typeof old_value === "object"){
+      Args.copy(old_value, new_value);
+      return old_value;
+    }
+    return new_value;
+  };
+
+  return CssHashSet;
+})();
---
+++ src/hash-set.js
@@ -0,0 +1,36 @@
+var HashSet = (function(){
+  function HashSet(values){
+    this._values = {};
+    if(values){
+      this.addValues(values);
+    }
+  }
+
+  HashSet.prototype = {
+    iter : function(fn){
+      Obj.iter(this._values, fn);
+    },
+    filter : function(fn){
+      return Obj.filter(this._values, fn);
+    },
+    // return merged value when conflict.
+    // simply overwrite by default.
+    merge : function(old_value, new_value){
+      return new_value;
+    },
+    add : function(name, value){
+      var old_value = this._values[name] || null;
+      this._values[name] = old_value? this.merge(old_value, value) : value;
+    },
+    addValues : function(values){
+      for(var prop in values){
+	this.add(prop, values[prop]);
+      }
+    }
+  };
+
+  return HashSet;
+})();
+
+
+
