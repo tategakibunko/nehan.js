1a03f5ff8ebdee8c36e1670ff5eeb0111cc1bcf2
tategakibunko <lambda.watanabe@gmail.com>
2014-06-29 12:37:06 +0900

    rename vars

--- build/nehan.js
+++ build/nehan.js
@@ -3907,10 +3907,10 @@ var Colors = (function(){
 
 var Palette = (function(){
   // 256(8 * 8 * 4) color palette scales.
-  var RG_PALETTE = [0, 36, 73, 109, 146, 182, 219, 255];
-  var B_PALETTE = [0, 85, 170, 255];
+  var __rg_palette = [0, 36, 73, 109, 146, 182, 219, 255];
+  var __b_palette = [0, 85, 170, 255];
 
-  var make_hex_str = function(ival){
+  var __make_hex_str = function(ival){
     var str = ival.toString(16);
     if(str.length <= 1){
       return "0" + str;
@@ -3918,7 +3918,7 @@ var Palette = (function(){
     return str;
   };
 
-  var find_palette = function(ival, palette){
+  var __find_palette = function(ival, palette){
     if(List.exists(palette, Closure.eq(ival))){
       return ival;
     }
@@ -3931,14 +3931,14 @@ var Palette = (function(){
     // search and return color value defined in nehan palette.
     // we use this value for img characters.
     getColor : function(rgb){
-      var palette_red = find_palette(rgb.getRed(), RG_PALETTE);
-      var palette_green = find_palette(rgb.getGreen(), RG_PALETTE);
-      var palette_blue = find_palette(rgb.getBlue(), B_PALETTE);
+      var palette_red = __find_palette(rgb.getRed(), __rg_palette);
+      var palette_green = __find_palette(rgb.getGreen(), __rg_palette);
+      var palette_blue = __find_palette(rgb.getBlue(), __b_palette);
 
       return [
-	make_hex_str(palette_red),
-	make_hex_str(palette_green),
-	make_hex_str(palette_blue)
+	__make_hex_str(palette_red),
+	__make_hex_str(palette_green),
+	__make_hex_str(palette_blue)
       ].join("");
     }
   };
