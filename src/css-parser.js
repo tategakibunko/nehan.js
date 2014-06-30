b8259c26b87e61e4f2b829cf3583ef245d296198
tategakibunko <lambda.watanabe@gmail.com>
2014-06-30 18:29:39 +0900

    rename local fun to private style

--- src/css-parser.js
+++ src/css-parser.js
@@ -40,37 +40,37 @@
   width
 */
 var CssParser = (function(){
-  var normalize = function(value){
+  var __normalize = function(value){
     return Utils.trim(String(value))
       .replace(/;/g, "")
       .replace(/\n/g, "");
   };
 
-  var split_space = function(value){
+  var __split_space = function(value){
     return (value.indexOf(" ") < 0)? [value] : value.split(/\s+/);
   };
 
-  var split_slash = function(value){
+  var __split_slash = function(value){
     return (value.indexOf("/") < 0)? [value] : value.split("/");
   };
 
   // props: [a,b,c]
   // values:[1,2,3]
   // => {a:1, b:2, c:3}
-  var zip_obj = function(props, values){
+  var __zip_obj = function(props, values){
     var ret = {};
     if(props.length !== values.length){
-      throw "invalid args:zip_obj";
+      throw "invalid args:__zip_obj";
     }
     List.iteri(props, function(i, prop){ ret[prop] = values[i]; });
     return ret;
   };
 
-  var get_map_2d = function(len){
+  var __get_map_2d = function(len){
     return Const.css2dIndex[Math.min(len, 2)] || [];
   };
 
-  var get_map_4d = function(len){
+  var __get_map_4d = function(len){
     return Const.css4dIndex[Math.min(len, 4)] || [];
   };
 
@@ -78,128 +78,128 @@ var CssParser = (function(){
   // map: [0,1,0,1]
   // => [values[0], values[1], values[0], values[1]]
   // => [a, b, a, b]
-  var make_values_by_map = function(values, map){
+  var __make_values_by_map = function(values, map){
     return List.map(map, function(index){ return values[index]; });
   };
 
   // values:[0] => [0,0]
   // values:[0,1] => [0,1]
-  var make_values_2d = function(values){
-    var map = get_map_2d(values.length);
-    return make_values_by_map(values, map);
+  var __make_values_2d = function(values){
+    var map = __get_map_2d(values.length);
+    return __make_values_by_map(values, map);
   };
 
   // values:[0] => [0,0,0,0],
   // values:[0,1] => [0, 1, 0, 1]
   // values:[0,2,3] => [0,1,2,1]
   // values:[0,1,2,3] => [0,1,2,3]
-  var make_values_4d = function(values){
-    var map = get_map_4d(values.length);
-    return make_values_by_map(values, map);
+  var __make_values_4d = function(values){
+    var map = __get_map_4d(values.length);
+    return __make_values_by_map(values, map);
   };
 
-  var make_edge_4d = function(values){
+  var __make_edge_4d = function(values){
     var props = Const.cssBoxDirsLogical; // len = 4
-    var values_4d = make_values_4d(values); // len = 4
+    var values_4d = __make_values_4d(values); // len = 4
     return List.zipObj(props, values_4d);
   };
 
-  var make_corner_4d = function(values){
+  var __make_corner_4d = function(values){
     var props = Const.cssBoxCornersLogical; // len = 4
-    var values_4d = make_values_4d(values); // len = 4
-    return zip_obj(props, values_4d);
+    var values_4d = __make_values_4d(values); // len = 4
+    return __zip_obj(props, values_4d);
   };
 
-  var parse_4d = function(value){
-    return make_edge_4d(split_space(value));
+  var __parse_4d = function(value){
+    return __make_edge_4d(__split_space(value));
   };
 
-  var parse_corner_4d = function(value){
-    var values_2d = make_values_2d(split_slash(value));
+  var __parse_corner_4d = function(value){
+    var values_2d = __make_values_2d(__split_slash(value));
     var values_4d_2d = List.map(values_2d, function(val){
-      return make_values_4d(split_space(val));
+      return __make_values_4d(__split_space(val));
     });
     var values = List.zip(values_4d_2d[0], values_4d_2d[1]);
-    return make_corner_4d(values);
+    return __make_corner_4d(values);
   };
 
-  var parse_border_abbr = function(value){
+  var __parse_border_abbr = function(value){
     var ret = [];
-    var values = split_space(value);
+    var values = __split_space(value);
     var arg_len = values.length;
     if(arg_len >= 1){
-      ret.push({"border-width":parse_4d(values[0])});
+      ret.push({"border-width":__parse_4d(values[0])});
     }
     return ret;
   };
 
-  var parse_list_style_abbr = function(value){
+  var __parse_list_style_abbr = function(value){
     var ret = [];
-    var values = split_space(value);
+    var values = __split_space(value);
     var arg_len = values.length;
     if(arg_len >= 1){
-      ret.push({"list-style-type":parse_4d(values[0])});
+      ret.push({"list-style-type":__parse_4d(values[0])});
     }
     if(arg_len >= 2){
-      ret.push({"list-style-image":parse_4d(values[1])});
+      ret.push({"list-style-image":__parse_4d(values[1])});
     }
     if(arg_len >= 3){
-      ret.push({"list-style-position":parse_4d(values[2])});
+      ret.push({"list-style-position":__parse_4d(values[2])});
     }
     return ret;
   };
 
-  var parse_font_abbr = function(value){
+  var __parse_font_abbr = function(value){
     return {}; // TODO
   };
 
-  var parse_background_abbr = function(value){
+  var __parse_background_abbr = function(value){
     return {}; // TODO
   };
 
   // all subdivided properties are evaluated as unified value.
   // for example, 'margin-before:1em' => 'margin:1em 0 0 0'.
   // so subdivided properties must be renamed to unified property('margin-before' => 'margin').
-  var format_prop = function(prop){
+  var __format_prop = function(prop){
     if(prop.indexOf("margin-") >= 0 || prop.indexOf("padding-") >= 0 || prop.indexOf("border-width-") >= 0){
       return prop.split("-")[0];
     }
     return prop;
   };
 
-  var format_value = function(prop, value){
+  var __format_value = function(prop, value){
     switch(typeof value){
     case "function": case "object": case "boolean":
       return value;
     }
-    value = normalize(value); // number, string
+    value = __normalize(value); // number, string
     switch(prop){
       /* TODO: border abbr
     case "border":
-      return parse_border_abbr(value);
+      return __parse_border_abbr(value);
       */
     case "border-width":
-      return parse_4d(value);
+      return __parse_4d(value);
     case "border-radius":
-      return parse_corner_4d(value);
+      return __parse_corner_4d(value);
     case "border-color":
-      return parse_4d(value);
+      return __parse_4d(value);
     case "border-style":
-      return parse_4d(value);
+      return __parse_4d(value);
 
       /* TODO: font abbr
     case "font":
-      return parse_font_abbr(value);
+      return __parse_font_abbr(value);
       */
 
       /* TODO: list-style abbr
     case "list-style":
-      return parse_list_style_abbr(value);
+      return __parse_list_style_abbr(value);
       */
     case "margin":
-      return parse_4d(value);
+      return __parse_4d(value);
     case "padding":
-      return parse_4d(value);
+      return __parse_4d(value);
 
     // subdivided properties
     case "margin-before": case "padding-before": case "border-width-before":
@@ -218,10 +218,10 @@ var CssParser = (function(){
 
   return {
     formatProp : function(prop){
-      return format_prop(prop);
+      return __format_prop(prop);
     },
     formatValue : function(prop, value){
-      return format_value(prop, value);
+      return __format_value(prop, value);
     }
   };
 })();
