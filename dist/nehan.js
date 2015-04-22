ca26786594051c8e244bb5af93cd99ca416fe49b
tategakibunko <lambda.watanabe@gmail.com>
2015-04-22 17:29:32 +0900

    create empty line immediately at block level

--- dist/nehan.js
+++ dist/nehan.js
@@ -14224,6 +14224,14 @@ var BlockGenerator = (function(){
       return this.yieldChildLayout(context);
     }
 
+    // empty line at block level
+    if(token.getName() === "br"){
+      return this.style.createLine({
+	//maxExtent:Math.floor(this.style.getAutoLineExtent())
+	maxExtent:this.style.getFontSize() // maybe this size is better
+      });
+    }
+
     // if tag token, inherit style
     var child_style = new StyleContext(token, this.style, {cursorContext:context});
 
