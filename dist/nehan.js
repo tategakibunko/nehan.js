253bd4b5649e3d1308b277b79052dbf2891c9e58
tategakibunko <lambda.watanabe@gmail.com>
2015-07-05 10:33:25 +0900

    update rex

--- dist/nehan.js
+++ dist/nehan.js
@@ -9359,7 +9359,7 @@ Nehan.HtmlLexer = (function (){
 
   HtmlLexer.prototype = {
     _normalize : function(src){
-      var src = src.replace(/(<\/.+?>)/gm, function(str, p1){
+      var src = src.replace(/(<\/[^>]+>)/gm, function(str, p1){
 	  return p1.toLowerCase();
       }); // convert close tag to lower case(for innerHTML of IE)
       src = __replace_single_close_tags(src);
