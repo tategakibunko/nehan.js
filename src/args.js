b626da5dcb827a0205a8c221b663d252457673cb
tategakibunko <lambda.watanabe@gmail.com>
2015-12-06 09:27:01 +0900

    Remove unused func

--- src/args.js
+++ src/args.js
@@ -17,24 +17,6 @@ Nehan.Args = {
     return dst;
   },
   /**
-     copy all value in [args] to [dst] recursively
-     @memberof Nehan.Args
-     @param {Object} dst
-     @param {Object} args
-     @return {Object} copied dst
-  */
-  copy2 : function(dst, args){
-    dst = dst || {};
-    for(var prop in args){
-      if(typeof dst[prop] === "object"){
-	this.copy2(dst[prop], args[prop]);
-      } else {
-	dst[prop] = args[prop];
-      }
-    }
-    return dst;
-  },
-  /**
      merge all value in [args] to [dst] with default value by [defaults]
      @memberof Nehan.Args
      @param {Object} dst
---
+++ tests/nehan.tag.js
@@ -0,0 +1,68 @@
+describe("Nehan.Tag", function(){
+  var tag = new Nehan.Tag("<div id='woo' class='hoge hige hage' data-name='taro' data-age='10' width='100'>");
+  it("Tag::getName", function(){
+    expect(tag.getName()).toBe("div");
+  });
+
+  it("Tag::getAlias", function(){
+    var tmp = tag.clone();
+    tmp.setAlias("foo");
+    expect(tmp.getName()).toBe("foo");
+  });
+
+  it("Tag::getPath", function(){
+    expect(tag.getPath()).toBe("div#woo.hoge.hige.hage");
+  });
+  
+  it("Tag::getAttr", function(){
+    expect(tag.getAttr("width")).toBe("100");
+  });
+
+  it("Tag::getClasses", function(){
+    expect(tag.getClasses()).toEqual(["hoge", "hige", "hage"]);
+  });
+
+  it("Tag::hasClass", function(){
+    expect(tag.hasClass("hoge")).toBe(true);
+    expect(tag.hasClass("hige")).toBe(true);
+    expect(tag.hasClass("hage")).toBe(true);
+    expect(tag.hasClass("hee")).toBe(false);
+  });
+  
+  it("Tag::getData", function(){
+    expect(tag.getData("name")).toEqual("taro");
+    expect(tag.getData("age")).toBe(10);
+  });
+
+  it("Tag::setAttr", function(){
+    var tmp = tag.clone();
+    tmp.setAttr("width", "200");
+    expect(tmp.getAttr("width")).toBe("200");
+    tmp.setAttr("height", 100); // as integer
+    expect(tmp.getAttr("height")).toBe(100);
+  });
+
+  it("Tag::setAttrs", function(){
+    var tmp = tag.clone();
+    tmp.setAttrs({
+      width:100,
+      height:200
+    });
+    expect(tmp.getAttr("width")).toBe(100);
+    expect(tmp.getAttr("height")).toBe(200);
+  });
+
+  it("Tag::addClass", function(){
+    var tmp = tag.clone();
+    tmp.addClass("foo");
+    expect(tmp.getClasses()).toContain("foo");
+  });
+
+  it("Tag::removeClass", function(){
+    var tmp = tag.clone();
+    tmp.removeClass("hoge");
+    expect(tmp.getClasses()).not.toContain("hoge");
+  });
+});
+
+
