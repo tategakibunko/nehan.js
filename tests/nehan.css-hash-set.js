dd7724210d3ce31da63b3f0491c54f5f64e93aa2
tategakibunko <lambda.watanabe@gmail.com>
2015-12-15 07:22:48 +0900

    Add test case

---
+++ tests/nehan.css-hash-set.js
@@ -0,0 +1,19 @@
+describe("Nehan::CssHashSet", function(){
+  it("CssHashSet.add(atom)", function(){
+    var set = new Nehan.CssHashSet();
+    set.add("foo", 10);
+    expect(set.get("foo")).toBe(10);
+    set.add("foo", 20);
+    expect(set.get("foo")).toBe(20);
+  });
+
+  it("CssHashSet.add(object)", function(){
+    var set = new Nehan.CssHashSet();
+    set.add("foo", {a:1});
+    expect(set.get("foo")).toEqual({a:1});
+    set.add("foo", {a:10});
+    expect(set.get("foo")).toEqual({a:10});
+    set.add("foo", {a:10, b:20});
+    expect(set.get("foo")).toEqual({a:10, b:20});
+  });
+});
