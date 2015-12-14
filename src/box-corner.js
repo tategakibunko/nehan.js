6b40921c37ecbf8971a3c890ea8aa8ed1abc2ef3
tategakibunko <lambda.watanabe@gmail.com>
2015-12-14 21:26:08 +0900

    Add jsdoc

--- src/box-corner.js
+++ src/box-corner.js
@@ -9,8 +9,13 @@ Nehan.BoxCorner = (function(){
     });
   };
   return {
-    getPhysicalCornerName : function(flow, logical_name){
-      var dirs = logical_name.split("-");
+    /**
+     @memberof Nehan.BoxCorner
+     @param flow {Nehan.BoxFlow}
+     @param corner_name {String} logical corner name ['before-start' | 'before-end' | 'after-end' | 'after-start']
+     */
+    getPhysicalCornerName : function(flow, corner_name){
+      var dirs = corner_name.split("-");
       var dir1 = flow.getProp(dirs[0]);
       var dir2 = flow.getProp(dirs[1]);
       return __sort(dir1, dir2).join("-");
