7388679cc67947a727965e30aef0abdec32d0b26
tategakibunko <lambda.watanabe@gmail.com>
2015-12-14 12:40:10 +0900

    add test for text-empha shorthand

---
+++ tests/nehan.css-text-empha-parser.js
@@ -0,0 +1,29 @@
+describe("Nehan::CssTextEmphaParser", function(){
+  var prop = new Nehan.CssProp("text-emphasis");
+
+  it("Nehan.formatValue(direct attr)", function(){
+    expect(Nehan.CssTextEmphaParser.formatValue(new Nehan.CssProp("text-emphasis-style"), "filled circle")).toEqual({style:"filled circle"});
+    expect(Nehan.CssTextEmphaParser.formatValue(new Nehan.CssProp("text-emphasis-position"), "over right")).toEqual({
+      position:{
+	hori:"over",
+	vert:"right"
+      }
+    });
+  });
+
+  it("Nehan.formatValue(shorthand)", function(){
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "none")).toEqual({});
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "filled")).toEqual({style:"filled"});
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "dot")).toEqual({style:"dot"});
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "'foo'")).toEqual({style:"'foo'"});
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "filled dot 'foo'")).toEqual({style:"'foo'"});
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, "filled dot red")).toEqual({
+      style:"filled dot",
+      color:"red"
+    });
+  });
+
+  it("Nehan.formatValue(object)", function(){
+    expect(Nehan.CssTextEmphaParser.formatValue(prop, {style:"filled circle"})).toEqual({style:"filled circle"});
+  });
+});
