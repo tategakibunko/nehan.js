41cf21505ac2d798f9a404a751c78e2391bf9997
tategakibunko <lambda.watanabe@gmail.com>
2015-12-17 18:58:32 +0900

    add test

--- tests/nehan.selector-lexer.js
+++ tests/nehan.selector-lexer.js
@@ -17,10 +17,32 @@ describe("SelectorLexer", function(){
 
   it("E > F(child combinator)", function(){
     var tokens = new Nehan.SelectorLexer("body > p.foo").getTokens();
-    console.log("child combinator:%o", tokens);
+    //console.log("child combinator:%o", tokens);
     expect(tokens.length).toBe(3);
     expect(tokens[0].name).toBe("body");
     expect(tokens[1]).toBe(">");
+    expect(tokens[2].name).toBe("p");
+    expect(tokens[2].classes.length).toBe(1);
+    expect(tokens[2].classes[0]).toBe("foo");
+  });
+
+  it("E + F(next-sibling combinator)", function(){
+    var tokens = new Nehan.SelectorLexer("body + p.foo").getTokens();
+    //console.log("next-sibling combinator:%o", tokens);
+    expect(tokens.length).toBe(3);
+    expect(tokens[0].name).toBe("body");
+    expect(tokens[1]).toBe("+");
+    expect(tokens[2].name).toBe("p");
+    expect(tokens[2].classes.length).toBe(1);
+    expect(tokens[2].classes[0]).toBe("foo");
+  });
+
+  it("E ~ F(following-sibling combinator)", function(){
+    var tokens = new Nehan.SelectorLexer("body ~ p.foo").getTokens();
+    console.log("following-sibling combinator:%o", tokens);
+    expect(tokens.length).toBe(3);
+    expect(tokens[0].name).toBe("body");
+    expect(tokens[1]).toBe("~");
     expect(tokens[2].name).toBe("p");
     expect(tokens[2].classes.length).toBe(1);
     expect(tokens[2].classes[0]).toBe("foo");
