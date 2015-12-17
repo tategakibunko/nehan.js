cd4c8a732cdda91bb7723c778eb8d0ba1f42f4e2
tategakibunko <lambda.watanabe@gmail.com>
2015-12-18 08:10:33 +0900

    add test

---
+++ tests/nehan.selector-state-machine.js
@@ -0,0 +1,7 @@
+describe("SelectorStateMachine", function(){
+  it("SelectorStateMachine.accept(E F G) for G", function(){
+  });
+
+  it("SelectorStateMachine.accept(E>F>G) for G", function(){
+  });
+});
